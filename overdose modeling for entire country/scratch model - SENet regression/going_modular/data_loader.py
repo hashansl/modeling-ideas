c2be30c2a2b0d01aaecb88156c667084da2281a9
hashansl <hashan.dan@gmail.com>
2024-06-12 08:25:06 -0400

    updated

---
+++ overdose modeling for entire country/scratch model - SENet regression/going_modular/data_loader.py
@@ -0,0 +1,75 @@
+
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+import pandas as pd
+import os
+from skimage import io
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms  # Transformations we can perform on our dataset
+from torchvision.transforms import ToPILImage
+
+
+class data_loader_persistence_img(Dataset):
+
+    def __init__(self,annotation_file_path,root_dir,transform=None):
+        self.annotations = pd.read_csv(annotation_file_path)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.class_names = sorted(self.annotations['percentile'].unique())
+        self.to_pil = ToPILImage()  # Initialize ToPILImage transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self,index):
+
+        npy_file_path = os.path.join(self.root_dir, str(self.annotations.iloc[index,0]) + '.npy')
+
+        img = np.load(npy_file_path).astype(np.float32)
+        # img = self.to_pil(img)
+
+        y_label = torch.tensor(int(self.annotations.iloc[index]['percentile']))
+
+        if self.transform:
+            img = self.transform(img)
+        return (img, y_label)
+
+    def get_class_names(self):
+        return self.class_names
+
+
+
+
+# root_dir = "/Users/h6x/ORNL/git/modeling-ideas/overdose modeling for entire country/results/persistence images/below 90th percentile/h1/npy_combined_features" # has 5 classes
+# annotation_file_path = "/Users/h6x/ORNL/git/modeling-ideas/overdose modeling for entire country/data/processed data/svi with hepvu/2018/annotation 2018/annotation.csv"
+
+# dataset = data_loader_persistence_img(annotation_file_path=annotation_file_path,root_dir=root_dir,transform=transforms.ToTensor())
+
+# print(len(dataset))
+
+# print(len(dataset[0]))
+
+# print(dataset[0][1])
+# print(dataset[0][0].shape)
+
+# train_set, test_set = torch.utils.data.random_split(dataset, [70, 25])
+
+
+# #---
+# train_data = DataLoader(dataset=train_set, batch_size=16, shuffle=True)
+# test_data = DataLoader(dataset=test_set, batch_size=16, shuffle=False)
+
+# class_names = dataset.get_class_names()
+# print(class_names)
+
+
+
+
+
+
+
---
+++ overdose modeling for entire country/scratch model - SENet regression/going_modular/train.py
@@ -0,0 +1,91 @@
+"""
+Trains a PyTorch image classification model using device-agnostic code.
+"""
+
+import os
+import torch
+import data_setup, engine, model_builder, utils,loss_and_accuracy_curve_plotter,testing
+
+
+from torchvision import transforms
+from timeit import default_timer as timer 
+
+# Setup hyperparameters
+NUM_EPOCHS = 10
+BATCH_SIZE = 8
+HIDDEN_UNITS = 20
+LEARNING_RATE = 0.001
+
+
+#going modular/data/pizza_steak_sushi/train
+# Setup directories
+root_dir = "/Users/h6x/ORNL/git/modeling-ideas/overdose modeling for entire country/results/persistence images/below 90th percentile/h1/npy_combined_features" # has 5 classes
+annotation_file_path = "/Users/h6x/ORNL/git/modeling-ideas/overdose modeling for entire country/data/processed data/svi with hepvu/2018/annotation 2018/annotation.csv"
+
+# Setup target device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Create transforms
+data_transform = transforms.Compose([
+#   transforms.Resize((64, 64)),
+  transforms.ToTensor()
+])
+
+# Create DataLoaders with help from data_setup.py
+train_dataloader, validation_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    annotation_file_path=annotation_file_path,
+    root_dir=root_dir,
+    transform=data_transform,
+    batch_size=BATCH_SIZE
+)
+
+# Create model with help from model_builder.py
+model = model_builder.TinyVGG(
+    input_shape=16,
+    hidden_units=HIDDEN_UNITS,
+    output_shape=len(class_names)
+).to(device)
+
+# Set loss and optimizer
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),
+                             lr=LEARNING_RATE)
+
+start_time = timer()
+
+# Start training with help from engine.py
+results = engine.train(model=model,
+             train_dataloader=train_dataloader,
+             validation_dataloader=validation_dataloader,
+             loss_fn=loss_fn,
+             optimizer=optimizer,
+             epochs=NUM_EPOCHS,
+             device=device,
+             use_mixed_precision=True,
+             save_name="tinyvgg_model.pth",
+             save_path="/Users/h6x/ORNL/git/persistence-image-classification/scratch model 1/models/")
+
+# End the timer and print out how long it took
+end_time = timer()
+print(f"Total training time: {end_time-start_time:.3f} seconds")
+
+#plotting the results
+loss_and_accuracy_curve_plotter.plot_loss_curves(results)
+
+# Test the model after training
+test_loss, test_acc = testing.test_step(model=model,
+                                  dataloader=test_dataloader,
+                                  loss_fn=loss_fn,
+                                  device=device,
+                                  use_mixed_precision=True)
+
+# Print out test results
+print(
+    f"Test results | "
+    f"test_loss: {test_loss:.4f} | "
+    f"test_acc: {test_acc:.4f}"
+)
+
+# # Update results dictionary with test results
+# results["test_loss"].append(test_loss)
+# results["test_acc"].append(test_acc)
